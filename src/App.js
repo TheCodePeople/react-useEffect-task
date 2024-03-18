c6d49ee476b2fe2d16817777a9875b5219e099d4
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-03-19 01:53:30 +0300

    Changed the CSS to Tailwind

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ const [userinfo , setuserinfo]=useState([])
   }, []);
 
   return <div>
-     <center><h1>User List</h1></center> 
+    <h1 className="p-10 text-center text-3xl font-bold text-blue-950 " >User List</h1>
 
-    <div className="contener">
+    <div className="flex flex-wrap justify-center items-center gap-10 w-full h-full p-10 ">
  {userinfo.map(user => (
-  <div className="card">
-    <h3>Name:{user.name}</h3>
+  <div className=" flex flex-col gap-4 w-{15%} h-40 p-2 text-center rounded-lg shadow-[-0px_1px_20px_1px_#00000024] cursor-pointer ">
+    <h3 className="font-bold">Name:{user.name}</h3>
     <p>Email: {user.email}</p>
     <p>Phone: {user.phone}</p>
     <p>Company: {user.company.name}</p>
