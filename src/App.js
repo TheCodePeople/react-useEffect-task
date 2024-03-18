d818ed3c5259d0f1ac6e6765e01c4d0e69e3b60f
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-03-18 23:35:15 +0300

    done task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
-
+import { useEffect,useState } from "react";
 function App() {
-  return <div></div>;
+const [userinfo , setuserinfo]=useState([])
+ async function getuserinfo (){
+   try {
+    const respons =await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!respons.ok) {return;}
+    const data =await respons.json()
+    setuserinfo(data)
+   } catch (error) {
+    console.log("is erorr",error);
+   }
+  };
+
+  useEffect(() => {
+    getuserinfo();
+  }, []);
+
+  return <div>
+     <center><h1>User List</h1></center> 
+
+    <div className="contener">
+ {userinfo.map(user => (
+  <div className="card">
+    <h3>Name:{user.name}</h3>
+    <p>Email: {user.email}</p>
+    <p>Phone: {user.phone}</p>
+    <p>Company: {user.company.name}</p>
+   </div>
+))}
+
+</div>
+    </div>
+ 
 }
 
 export default App;
