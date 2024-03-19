4a7d2070abff2bf88d114fb35a04bde037a172aa
zainabthabit <159033487+zainabthabit@users.noreply.github.com>
2024-03-19 20:55:09 +0300

    solving user list without styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import { useState, useEffect } from 'react';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [userList,setUserList]=useState([])
+
+  const fetchUserList = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+       const data = await response.json();
+       setUserList(data)
+       console.log(data)  
+  } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchUserList();
+  }, []);
+
+  return (
+    <div>
+    <ul>
+        {userList?.map((user) => (
+          <li style={{border:"2px solid black", backgroundColor: "lightblue"}}
+          key={user.id}>user name  : {user.name}  
+          <ul>email  : {user.email}</ul>
+          <ul>phone number  : {user.phone}</ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
