995cb6f69a0404eb9cba25b9bbf68ef5256b957f
mehdi22256 <mehdi22256@gmail.com>
2024-03-20 14:27:12 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
-
+import { Card } from "./components/Card";
+import useFetch from "./hooks/useFetch";
 function App() {
-  return <div></div>;
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/users");
+
+  return (
+    <div>
+      {data.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="flex flex-wrap gap-5 justify-center items-center pt-10 ">
+          {data.map((user) => (
+            <Card key={user.id} user={user} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
